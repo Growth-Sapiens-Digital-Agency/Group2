1c9e20432468484cd50965005b22a0d084588608
Stephen-GS <stephen@growthsapiens.com>
2021-08-31 13:23:49 +0530

    code add to footer from index file

--- wp-content/themes/astra/footer.php
+++ wp-content/themes/astra/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -10,48 +11,106 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+
+
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
 ?>
-<?php astra_content_bottom(); ?>
-        
-	</div> <!-- ast-container -->
-	</div><!-- #content -->
-<?php 
-	astra_content_after();
-		
-	astra_footer_before();
-		
-	astra_footer();
-		
-	astra_footer_after(); 
-	
-?>
 
-   <!-- Social media icons -->
+<style>
+	/* Stephen */
+	.newsletter-block {
+		text-align: center;
+		background: #17181d;
+	}
+
+	.two-col {
+		width: 100%;
+		display: -webkit-box;
+		display: -moz-box;
+		display: -webkit-flex;
+		display: -ms-flexbox;
+		display: flex;
+	}
+
+	.half-col {
+		width: 50%;
+	}
+
+	.half-col.lrg {
+		width: 70%;
+	}
+
+	.newsletter-block input {
+		width: 100%;
+	}
+
+	.social_icons_image {
+		height: 30;
+		width: 30px;
+	}
+</style>
+<?php astra_content_bottom(); ?>
+
+</div> <!-- ast-container -->
+</div><!-- #content -->
+<?php
+astra_content_after();
+
+astra_footer_before();
+
+astra_footer();
+
+astra_footer_after();
+
+?>
+<!-- Newsletter block | Stephen-->
 <div class="container">
-<div class="row" style="text-align:center">
-    <div class="col-sm-12" style="background-color:#EEEEEE;">
-    <div class="social-icon-container">
-<a href="#"><img  class="social_icons_image" src="assets/images/twitter.png" alt="Twitter Logo"> </a>
-             
-<a href="#"><img class="social_icons_image" src="assets/images/facebook.png" alt="Facebook Logo"> </a>
-<a href="#"><img class="social_icons_image" src="assets/images/youtube.png" alt="youtube Logo"> </a>
-<a href="#"><img class="social_icons_image" src="assets/images/linkidin.png" alt="linkedin logo"> </a>
-<a href="#"><img  class="social_icons_image" src="assets/images/google.png" alt="google logo"> </a>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="newletter-block">
+				<h2>Subscribe To Our Newsletter</h2>
+				<p>Sign up here to get the latest news, updates and special offers delivered directly to your inbox.</p>
+				<form>
+					<div class="two-col">
+						<div class="half-col lrg">
+							<input type="text" placeholder="Enter Your Email ID" required>
+						</div>
+						<div class="half-col">
+							<button type="submit">subscribe<button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
-    </div>
-	</div>	
+
+<!-- Social media icons -->
+<div class="container">
+	<div class="row" style="text-align:center">
+		<div class="col-sm-12" style="background-color:#EEEEEE;">
+			<div class="social-icon-container">
+				<a href="#"><img class="social_icons_image" src="assets/images/twitter.png" alt="Twitter Logo"> </a>
+
+				<a href="#"><img class="social_icons_image" src="assets/images/facebook.png" alt="Facebook Logo"> </a>
+				<a href="#"><img class="social_icons_image" src="assets/images/youtube.png" alt="youtube Logo"> </a>
+				<a href="#"><img class="social_icons_image" src="assets/images/linkidin.png" alt="linkedin logo"> </a>
+				<a href="#"><img class="social_icons_image" src="assets/images/google.png" alt="google logo"> </a>
+			</div>
+		</div>
+	</div>
 </div>
 
 <!-- Social media icons end -->
-   
-	</div><!-- #page -->
-<?php 
-	astra_body_bottom();    
-	wp_footer(); 
+
+</div><!-- #page -->
+<?php
+astra_body_bottom();
+wp_footer();
 ?>
-	</body>
+</body>
+
 </html>
--- wp-content/themes/astra/index.php
+++ wp-content/themes/astra/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file.
  *
@@ -15,8 +16,8 @@
 
 
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 get_header(); ?>
@@ -25,28 +26,29 @@ get_header(); ?>
         width: 100%;
         overflow: hidden;
     }
-    
+
     .ticker-wrap1 {
         width: 100%;
         padding-left: 100%;
         background-color: #6b6767;
-        color:#fff;
-        padding:10px;
+        color: #fff;
+        padding: 10px;
     }
+
     .ticker-wrap2 {
         width: 100%;
         padding-left: 100%;
         background-color: orange;
-        color:#fff;
-        padding:10px;
+        color: #fff;
+        padding: 10px;
     }
-    
+
     @keyframes ticker {
         100% {
             transform: translate3d(-100%, 0, 0);
         }
     }
-    
+
     .ticker-move {
         display: inline-block;
         white-space: nowrap;
@@ -54,86 +56,73 @@ get_header(); ?>
         animation-timing-function: linear;
         animation: ticker 60s infinite;
     }
-    
+
     .ticker-move:hover {
         animation-play-state: paused;
     }
-    
+
     .ticker-item {
         display: inline-block;
         padding: 0 40px;
         font-size: 20px;
     }
+
+    
 </style>
 
-<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+<?php if (astra_page_layout() == 'left-sidebar') : ?>
 
-	<?php get_sidebar(); ?>
+    <?php get_sidebar(); ?>
 
 <?php endif ?>
-	<div id="primary" <?php astra_primary_class(); ?>>
-		<?php 
-		astra_primary_content_top();
-		
-		astra_content_loop();
+<div id="primary" <?php astra_primary_class(); ?>>
+    <?php
+    astra_primary_content_top();
 
-		astra_pagination();
+    astra_content_loop();
 
-		astra_primary_content_bottom(); 
-		?>
-	</div><!-- #primary -->
-<!-- Newsletter block -->
-	<div class="newletter-block">
-<h2>Subscribe To Our Newsletter</h2>
-<p>Sign up here to get the latest news, updates and special offers delivered directly to your inbox.</p>
-<form>
-			<div class="two-col">
-				<div class="half-col lrg">
-					<input type="text" placeholder="Enter Your Email ID" required>
-				</div>
-				<div class="half-col">
-					<button type="submit">subscribe<button>
-				</div>
-			</div>
-</form>
-	</div>
-<?php 
-if ( astra_page_layout() == 'right-sidebar' ) :
+    astra_pagination();
 
-	get_sidebar();
+    astra_primary_content_bottom();
+    ?>
+</div><!-- #primary -->
+
+<?php
+if (astra_page_layout() == 'right-sidebar') :
+
+    get_sidebar();
 
 endif;
 ?>
 <div class="footer-menu-section">
-<ul>
-<li><a href="#">Allergens & Colors</a></li>
-<li><a href="#">Privacy Policy</a></li>
-<li><a href="#">Terms of Use</a></li>
-<li><a href="#">Return Policy</a></li>
-<li><a href="#">Contact Us</a></li>
-<li><a href="#">Accessibility Standards</a></li>
-<li><a href="#">Accessibility for customers with a disability</a></li>
-</ul>
+    <ul>
+        <li><a href="#">Allergens & Colors</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms of Use</a></li>
+        <li><a href="#">Return Policy</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Accessibility Standards</a></li>
+        <li><a href="#">Accessibility for customers with a disability</a></li>
+    </ul>
 </div>
-  <?php
+<?php
 get_footer();
 ?>
- <div class="tcontainer">
-        <div class="ticker-wrap1">
-            <div class="ticker-move">
-                <div class="ticker-item">For a limited time, get 30% off Morpheus 8. </div>
-                <div class="ticker-item">Call us to claim your offer   </div>
-                
-            </div>
+<div class="tcontainer">
+    <div class="ticker-wrap1">
+        <div class="ticker-move">
+            <div class="ticker-item">For a limited time, get 30% off Morpheus 8. </div>
+            <div class="ticker-item">Call us to claim your offer </div>
+
         </div>
     </div>
-     <div class="tcontainer">
-        <div class="ticker-wrap2">
-            <div class="ticker-move">
-                <div class="ticker-item">20% off All Alumier products and fillers. Limited Period. </div>
-                <div class="ticker-item"> Call us on <a href="tel:289.337.2761">289.337.2761</a> to make a purchase</div>
-                
-            </div>
+</div>
+<div class="tcontainer">
+    <div class="ticker-wrap2">
+        <div class="ticker-move">
+            <div class="ticker-item">20% off All Alumier products and fillers. Limited Period. </div>
+            <div class="ticker-item"> Call us on <a href="tel:289.337.2761">289.337.2761</a> to make a purchase</div>
+
         </div>
     </div>
-  
+</div>
